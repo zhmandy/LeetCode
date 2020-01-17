2ed08518508fe5bed43334d31caa453e6799367e
zhmandy <zhmandy@outlook.com>
2020-01-16 19:14:52 -0500

    35 Search Insert Position

--- 34 Find First and Last Position of Element in Sorted Array.java
+++ 34 Find First and Last Position of Element in Sorted Array.java
@@ -1,4 +1,8 @@
 class Solution {
+    // 跑两个binary search来找start和end
+    // starting pos, 当nums[mid]大于等于target的时候, 一直减right, 如果最后right valid就是starting pos
+    // ending pos, 当nums[mid]小于等于target的时候, 一直加left, 如果最后left valid就是ending pos
+    // 注意edge cases
     public int[] searchRange(int[] nums, int target) {
         int[] result = {-1, -1};
         if (nums == null || nums.length == 0) return result;
---
+++ 35 Search Insert Position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
