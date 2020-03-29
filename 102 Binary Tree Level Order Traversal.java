a6c3134ea22423a6361c6b0b50476d52002af428
zhmandy <zhmandy@outlook.com>
2020-03-28 22:32:30 -0400

    102 Binary Tree Level Order Traversal

--- 102 Binary Tree Level Order Traversal.java
+++ 102 Binary Tree Level Order Traversal.java
@@ -12,26 +12,28 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
         
-        Queue<TreeNode> queue = new LinkedList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
         
         while (!queue.isEmpty()) {
             List<Integer> level = new ArrayList<>();
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                TreeNode head = queue.poll();
-                if (head != null) {
-                    level.add(head.val);
-                    if (head.left != null) {
-                        queue.offer(head.left);
-                    }
-                    if (head.right != null) {
-                        queue.offer(head.right);
-                    }
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                
+                if (node.right != null) {
+                    queue.add(node.right);
                 }
             }
+            
             result.add(level);
         }
+        
         return result;
     }
 }
