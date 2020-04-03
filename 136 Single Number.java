f51d7b04cba89d459db4ac8ed829bd788af74d7b
zhmandy <zhmandy@outlook.com>
2020-04-03 16:55:29 -0400

    136 Single Number

---
+++ 136 Single Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    // i^i=0
+    // i^0=i
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
