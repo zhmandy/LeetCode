cdcc7d75699effe868eade40a113a24b71d9510c
zhmandy <zhmandy@outlook.com>
2020-04-28 13:32:26 -0400

    206 reverse linked list

--- 206 Reverse Linked List.java
+++ 206 Reverse Linked List.java
@@ -13,3 +13,16 @@ class Solution {
         return head;
     }
 }
+
+// recursion
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode ret = reverseList(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+        
+        return ret;
+    }
+}
