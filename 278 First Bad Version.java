6efefbad9898ec139cb6ccf457b117c5f2d475b8
zhmandy <zhmandy@outlook.com>
2020-05-01 09:44:40 -0400

    278 First Bad Version

---
+++ 278 First Bad Version.java
@@ -0,0 +1,17 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if (n <= 1) return n;
+        int left = 1;
+        int right = n;
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid))
+                right = mid;
+            else
+                left = mid;
+        }
+        
+        return isBadVersion(left) ? left : right;
+    }
+}
