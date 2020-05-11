c020bfc215587b10a009ffa94646bff8123f27ad
zhmandy <zhmandy@outlook.com>
2020-05-11 17:02:46 -0400

    7 Reverse Integer

---
+++ 7 Reverse Integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    // check if its already overflow
+    public int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            
+            if (ret > Integer.MAX_VALUE / 10 || (ret == Integer.MAX_VALUE && pop > 7))
+                return 0;
+            if (ret < Integer.MIN_VALUE / 10 || (ret == Integer.MIN_VALUE && pop < -8))
+                return 0;
+            
+            ret = ret * 10 + pop;
+        }
+        
+        return ret;
+    }
+}
