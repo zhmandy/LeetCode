92826c0e20a938d86ce1e64fe72aab4e285da620
zhmandy <zhmandy@outlook.com>
2020-05-23 14:32:17 -0400

    14 Longest Common Prefix

---
+++ 14 Longest Common Prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    // vertical comparison
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                // if strs[j] length smaller than strs[0] or no common prefix
+                if (strs[j].length() == i || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        
+        return strs[0];
+    }
+}
