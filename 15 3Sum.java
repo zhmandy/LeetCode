fd32af80d35113ae428b08e407ee21aae0bdbc71
zhmandy <zhmandy@outlook.com>
2020-05-23 21:20:40 -0400

    15 3Sum

---
+++ 15 3Sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    // two pointers
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null || nums.length == 0) return ret;
+        
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum > target || (right < nums.length - 1 && nums[right] == nums[right + 1])) {
+                    right--;
+                } else if (sum < target || (left > i + 1 && nums[left] == nums[left - 1])) {
+                    left++;
+                } else {
+                    ret.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                }
+            }
+        }
+        
+        return ret;
+    }
+}
