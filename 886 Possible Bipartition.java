7c4201e41077f6c8aaa3fd4609df21b18eb4938d
zhmandy <zhmandy@outlook.com>
2020-05-28 23:48:41 -0400

    886 Possible Bipartition

--- 886 Possible Bipartition.java
+++ 886 Possible Bipartition.java
@@ -1,15 +1,16 @@
 class Solution {
+    // 和785相比就是多了一步自己整合数据成方便的结构
     public boolean possibleBipartition(int N, int[][] dislikes) {
         if (N < 0) return false;
         if (dislikes == null || dislikes.length == 0 || dislikes[0].length == 0) return true;
         
-        HashMap<Integer, Set<Integer>> graph = new HashMap<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
         // add dislike neighbors
         for (int[] d : dislikes) {
             int a = d[0];
             int b = d[1];
-            graph.putIfAbsent(a, new HashSet<>());
-            graph.putIfAbsent(b, new HashSet<>());
+            graph.putIfAbsent(a, new ArrayList<>());
+            graph.putIfAbsent(b, new ArrayList<>());
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
@@ -23,7 +24,7 @@ class Solution {
         return true;
     }
     
-    private boolean dfs(int[] colors, int node, int color, HashMap<Integer, Set<Integer>> graph) {
+    private boolean dfs(int[] colors, int node, int color, HashMap<Integer, List<Integer>> graph) {
         if (colors[node] != 0) {
             return color == colors[node];
         }
