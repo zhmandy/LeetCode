93de5bfb506edffe6c8bebfd7701d450e7c9415b
zhmandy <zhmandy@outlook.com>
2020-06-10 20:37:11 -0400

    35 add one more approach

--- 35 Search Insert Position.java
+++ 35 Search Insert Position.java
@@ -19,3 +19,27 @@ class Solution {
         return left;
     }
 }
+
+// approach 2
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        
+        if (nums[right] < target) return right + 1;
+        else if (nums[left] >= target) return left;
+        else return right;
+    }
+}
