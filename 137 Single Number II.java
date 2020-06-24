705c90cfb23bba4ef9170c4f68ad913709c5942a
zhmandy <zhmandy@outlook.com>
2020-06-23 22:33:07 -0400

    137 Single Number II

---
+++ 137 Single Number II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int num : map.keySet()) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        
+        return -1;
+    }
+}
