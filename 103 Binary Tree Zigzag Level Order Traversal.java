dd33dd67f5b983787932bd692d58c1e197df6ed1
zhmandy <zhmandy@outlook.com>
2020-08-18 11:25:20 -0400

    103 Binary Tree Zigzag Level Order Traversal

--- 103 Binary Tree Zigzag Level Order Traversal.java
+++ 103 Binary Tree Zigzag Level Order Traversal.java
@@ -1,39 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    // BFS遍历, 偶数行reverse
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
         boolean flag = false;
         
         while (!queue.isEmpty()) {
             List<Integer> level = new ArrayList<>();
             int size = queue.size();
+            
             for (int i = 0; i < size; i++) {
-                TreeNode head = queue.poll();
-                if (head != null) {
-                    level.add(head.val);
-                    if (head.left != null) {
-                        queue.offer(head.left);
-                    }
-                    if (head.right != null) {
-                        queue.offer(head.right);
-                    }
+                TreeNode current = queue.poll();
+                if (current != null) {
+                    level.add(current.val);
+                    if (current.left != null) queue.offer(current.left);
+                    if (current.right != null) queue.offer(current.right);
                 }
             }
-
-            // or use stack
+            
             if (flag) {
                 Collections.reverse(level);
                 result.add(level);
