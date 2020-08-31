ce84e64b2f4f2b26182792e5ee0f5209328bcc38
zhmandy <zhmandy@outlook.com>
2020-08-31 11:47:06 -0400

    258 Add Digits

---
+++ 258 Add Digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        int root = 0;
+        while (num > 0) {
+            root += num % 10;
+            num = num / 10;
+            
+            if (num == 0 && root > 9) {
+                num = root;
+                root = 0;
+            }
+        }
+        
+        return root;
+    }
+}
