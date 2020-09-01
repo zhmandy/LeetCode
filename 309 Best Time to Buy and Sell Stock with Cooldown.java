2fed75bfcf970f0228c09ada8618d71ca4f201bc
zhmandy <zhmandy@outlook.com>
2020-09-01 17:25:37 -0400

    309 Best Time to Buy and Sell Stock with Cooldown

--- 309 Best Time to Buy and Sell Stock with Cooldown.java
+++ 309 Best Time to Buy and Sell Stock with Cooldown.java
@@ -12,9 +12,9 @@ class Solution {
         sellProfit[1] = Math.max(0, buyProfit[0] + prices[1]);
         
         for (int i = 2; i < prices.length; i++) {
-            // maximum profit of do nothing or buy again based on the sellProfit day i-2
+            // maximum profit of do nothing or buy again based on the sellProfit at/before day i-2
             buyProfit[i] = Math.max(buyProfit[i - 1], sellProfit[i - 2] - prices[i]);
-            // maximum profit of do nothing or sell the stock bought yesterday
+            // maximum profit of do nothing or sell the stock based on buyProfit at/before day i-1
             sellProfit[i] = Math.max(sellProfit[i - 1], buyProfit[i - 1] + prices[i]);
         }
         
