b296297f8f93073664d456e6a5902b4cd01bd2ce
zhmandy <zhmandy@outlook.com>
2020-09-08 23:10:40 -0400

    886 Possible Bipartition

--- 886 Possible Bipartition.java
+++ 886 Possible Bipartition.java
@@ -6,6 +6,9 @@ class Solution {
         
         HashMap<Integer, List<Integer>> graph = new HashMap<>();
         // add dislike neighbors
+        // 需要将数组的两个都作为key, 在这种条件下才可以在下面的for loop把还没处理的数字预先分在1这个color
+        // 否则如果是这个例子[[1,3],[2,4],[3,4]], 如果只有单个key, 4就不是一个key, 在for loop中2会预先被分到1,
+        // 但是正确来说从4出发, 2应该被归为-1类, 这样就给出错误答案
         for (int[] d : dislikes) {
             int a = d[0];
             int b = d[1];
